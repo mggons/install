d74ce6e00af6f6a9a3a297f965e75d9b1ad507f1
mggons <malagonsxp@hotmail.com>
2022-09-22 09:18:30 -0500

    Update wininstall.ps1

--- wininstall.ps1
+++ wininstall.ps1
@@ -6,7 +6,7 @@ $wshell = New-Object -ComObject Wscript.Shell
 $Button = [System.Windows.MessageBoxButton]::YesNoCancel
 $ErrorIco = [System.Windows.MessageBoxImage]::Error
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs -WindowStyle Maximized
 	Exit
 }
 
