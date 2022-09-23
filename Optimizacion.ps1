cd4b6cb2be10ab562c796d3876dedd2b9ede4d9f
mggons <malagonsxp@hotmail.com>
2022-09-23 17:31:14 -0500

    Update Optimizacion.ps1

--- Optimizacion.ps1
+++ Optimizacion.ps1
@@ -423,15 +423,15 @@ Write-Host "Desactivando Hibernación..."
     }
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings" -Name "ShowHibernateOption" -Type Dword -Value 0
 
-Write-Host "Mostrando detalles del administrador de tareas..."
-    $taskmgr = Start-Process -WindowStyle Hidden -FilePath taskmgr.exe -PassThru
-    Do {
-        Start-Sleep -Milliseconds 100
-        $preferences = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager" -Name "Preferences" -ErrorAction SilentlyContinue
-    } Until ($preferences)
-    Stop-Process $taskmgr
-    $preferences.Preferences[28] = 0
-    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager" -Name "Preferences" -Type Binary -Value $preferences.Preferences
+#Write-Host "Mostrando detalles del administrador de tareas..."
+#    $taskmgr = Start-Process -WindowStyle Hidden -FilePath taskmgr.exe -PassThru
+#    Do {
+#        Start-Sleep -Milliseconds 100
+#       $preferences = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager" -Name "Preferences" -ErrorAction SilentlyContinue
+#    } Until ($preferences)
+#    Stop-Process $taskmgr
+#    $preferences.Preferences[28] = 0
+#    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager" -Name "Preferences" -Type Binary -Value $preferences.Preferences
 
 Write-Host "Ocultar el botón Vista de tareas..."
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Type DWord -Value 0
