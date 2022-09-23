71c4b55efb73e320e4720440f71933bb46b8db86
mggons <malagonsxp@hotmail.com>
2022-09-23 12:36:12 -0500

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -20,61 +20,6 @@ Add-AppxPackage -Path C:\Cambios\Postinstall\appx\Microsoft.UI.Xaml.2.7_7.2109.1
 Add-AppxPackage -Path C:\Cambios\Postinstall\appx\Microsoft.UI.Xaml.2.7_7.2109.13004.0_x86.appx
 Add-AppxPackage -Path C:\Cambios\Postinstall\appx\Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64.appx
 
-# GUI Specs
-Write-Host "Checking winget..."
-
-Try{
-	# Check if winget is already installed
-	$er = (invoke-expression "winget -v") 2>&1
-	if ($lastexitcode) {throw $er}
-	Write-Host "winget is already installed."
-}
-Catch{
-	# winget is not installed. Install it from the Github release
-	Write-Host "winget is not found, installing it right now."
-
-	$asset = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' | ForEach-Object assets | Where-Object name -like "*.msixbundle"
-	$output = $PSScriptRoot + "\winget-latest.appxbundle"
-	Write-Host "Downloading latest winget release"
-	Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $output
-
-	Write-Host "Installing the winget package"
-	Add-AppxPackage -Path $output
-
-    Write-Host "Cleanup winget install package"
-    if (Test-Path -Path $output) {
-        Remove-Item $output -Force -ErrorAction SilentlyContinue
-    }
-}
-
-
-# GUI Specs
-Write-Host "Checking winget..."
-
-Try{
-	# Check if winget is already installed
-	$er = (invoke-expression "winget -v") 2>&1
-	if ($lastexitcode) {throw $er}
-	Write-Host "winget is already installed."
-}
-Catch{
-	# winget is not installed. Install it from the Github release
-	Write-Host "winget is not found, installing it right now."
-
-	$asset = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' | ForEach-Object assets | Where-Object name -like "*.msixbundle"
-	$output = $PSScriptRoot + "\winget-latest.appxbundle"
-	Write-Host "Downloading latest winget release"
-	Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $output
-
-	Write-Host "Installing the winget package"
-	Add-AppxPackage -Path $output
-
-    Write-Host "Cleanup winget install package"
-    if (Test-Path -Path $output) {
-        Remove-Item $output -Force -ErrorAction SilentlyContinue
-    }
-}
-
 
 $delay = 15
 $Counter_Form = New-Object System.Windows.Forms.Form
