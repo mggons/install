a1d88cbdb3da0c29fbf593508c27643d37d0014f
mggons <malagonsxp@hotmail.com>
2022-09-23 12:34:50 -0500

    Update wininstall.ps1

--- wininstall.ps1
+++ wininstall.ps1
@@ -10,6 +10,60 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 	Exit
 }
 
+# GUI Specs
+Write-Host "Checking winget..."
+
+Try{
+	# Check if winget is already installed
+	$er = (invoke-expression "winget -v") 2>&1
+	if ($lastexitcode) {throw $er}
+	Write-Host "winget is already installed."
+}
+Catch{
+	# winget is not installed. Install it from the Github release
+	Write-Host "winget is not found, installing it right now."
+
+	$asset = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' | ForEach-Object assets | Where-Object name -like "*.msixbundle"
+	$output = $PSScriptRoot + "\winget-latest.appxbundle"
+	Write-Host "Downloading latest winget release"
+	Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $output
+
+	Write-Host "Installing the winget package"
+	Add-AppxPackage -Path $output
+
+    Write-Host "Cleanup winget install package"
+    if (Test-Path -Path $output) {
+        Remove-Item $output -Force -ErrorAction SilentlyContinue
+    }
+}
+
+
+# GUI Specs
+Write-Host "Checking winget..."
+
+Try{
+	# Check if winget is already installed
+	$er = (invoke-expression "winget -v") 2>&1
+	if ($lastexitcode) {throw $er}
+	Write-Host "winget is already installed."
+}
+Catch{
+	# winget is not installed. Install it from the Github release
+	Write-Host "winget is not found, installing it right now."
+
+	$asset = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' | ForEach-Object assets | Where-Object name -like "*.msixbundle"
+	$output = $PSScriptRoot + "\winget-latest.appxbundle"
+	Write-Host "Downloading latest winget release"
+	Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $output
+
+	Write-Host "Installing the winget package"
+	Add-AppxPackage -Path $output
+
+    Write-Host "Cleanup winget install package"
+    if (Test-Path -Path $output) {
+        Remove-Item $output -Force -ErrorAction SilentlyContinue
+    }
+}
 
 # $inputXML = Get-Content "MainWindow.xaml" #uncomment for development
 $inputXML = (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/mggons/install/main/MainWindow.xaml") #uncomment for Production
