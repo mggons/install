a4400fdf1198cbf403f9b5b20d584310225372cb
mggons <malagonsxp@hotmail.com>
2023-03-04 15:08:39 -0500

    Update Customize.ps1

--- Customize.ps1
+++ Customize.ps1
@@ -27,7 +27,7 @@ Write-Host "------------------------------------"
 "ping 127.0.0.1 -n 4 > nul" | cmd
 
 
-cd :\ | cmd
+cd C:\
 New-Item ODT -Type Directory
     Import-Module BitsTransfer
     Start-BitsTransfer -Source "http://www.aionlatam.com/files/Setup_Adguard.exe" -Destination C:\ODT\Setup_Adguard.exe
