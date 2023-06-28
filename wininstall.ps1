ac955cce00d77cb839459bcbf2b4a522b965815a
mggons <malagonsxp@hotmail.com>
2023-06-28 11:56:34 -0500

    Update wininstall.ps1

--- wininstall.ps1
+++ wininstall.ps1
@@ -15,6 +15,22 @@ Install-PackageProvider -Name Nuget -Force
 Install-Module PSWindowsUpdate -Force
 Hide-WindowsUpdate -KBArticleID KB5005463 -Confirm:$False
 
+# get latest download url
+$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+        Select-Object -ExpandProperty "assets" |
+        Where-Object "browser_download_url" -Match '.msixbundle' |
+        Select-Object -ExpandProperty "browser_download_url"
+
+# download
+Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
+
+# install
+Add-AppxPackage -Path "Setup.msix"
+
+# delete file
+Remove-Item "Setup.msix"
+
 # $inputXML = Get-Content "MainWindow.xaml" #uncomment for development
 $inputXML = (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/mggons/install/main/MainWindow.xaml") #uncomment for Production
 
