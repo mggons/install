c8491eb419418bb1f255d276608197bed04c1874
mggons <malagonsxp@hotmail.com>
2023-07-29 23:52:48 -0500

    Update WinInstall10.ps1
    Instalación de programas para Windows 10
    
    Ediciones:
    Windows 10 HSL
    Windows 10 H
    Windows 10 Pro
    Windows 10 Pro SL 
    Windows 10 Enterprise
    Windows 10 Enterprise LTSC 2019
    Windows 10 Pro for Workstation
    Windows 10 Education

--- WinInstall10.ps1
+++ WinInstall10.ps1
@@ -28,40 +28,15 @@ Try{
 Catch{
 	# winget is not installed. Install it from the Github release
 	Write-Host "winget is not found, installing it right now."
-
-	$asset = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' | ForEach-Object assets | Where-Object name -like "*.msixbundle"
-	$output = $PSScriptRoot + "\winget-latest.appxbundle"
-	Write-Host "Downloading latest winget release"
-	Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $output
-
-	Write-Host "Installing the winget package"
-	Add-AppxPackage -Path $output
-
-    Write-Host "Cleanup winget install package"
-    if (Test-Path -Path $output) {
-        Remove-Item $output -Force -ErrorAction SilentlyContinue
-    }
-}
-# GUI Specs
-Write-Host "Checking winget..."
-
-Try{
-	# Check if winget is already installed
-	$er = (invoke-expression "winget -v") 2>&1
-	if ($lastexitcode) {throw $er}
-	Write-Host "winget is already installed."
-}
-Catch{
-	# winget is not installed. Install it from the Github release
-	Write-Host "winget is not found, installing it right now."
-
-	$asset = Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' | ForEach-Object assets | Where-Object name -like "*.msixbundle"
-	$output = $PSScriptRoot + "\winget-latest.appxbundle"
-	Write-Host "Downloading latest winget release"
-	Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $output
-
-	Write-Host "Installing the winget package"
-	Add-AppxPackage -Path $output
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
+$latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
+Write-Information "Downloading winget to artifacts directory..."
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Write-Host "Installing the winget package"
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $latestWingetMsixBundle
 
     Write-Host "Cleanup winget install package"
     if (Test-Path -Path $output) {
